c7a8ea27115c4805b751260dc4f42f3f0de2717b
Pritam <pritamjad@gmail.com>
2024-12-29 00:04:11 +0530

    js new commit by dev

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-// Fetch and Display Data from API
-async function fetchData() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-        const data = await response.json();
-        console.log('Post Title:', data.title);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-// Example Usage
-fetchData();
-
+sorry sir leaving the job
 
